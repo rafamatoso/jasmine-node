8e0a64ad9a8361193b68af140b12a65a77fc37b0
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-05 21:04:17 -0300

    Integrando Travis CI ao projeto

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo com NodeJS", () => {
+  it("deve demonstrar o uso do Jasmine com o NodeJS", () => {
+    expect(true).toEqual(true);
+  });
+
+  it("deve demonstrar o uso do Karma em modo de escuta", () => {
+    expect(1).toEqual(1);
+  });
+});
